5361d3987b3b5afa3ca971690021c5f5d9b510ba
Adam Johnson <ajohnson@us-c02x84u9jgh5.opbdesktop.multiservice.com>
2018-10-16 16:26:53 -0500

    Adds three button layout for ball dispensers with event handlers.

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,6 +1,16 @@
-let btn = document.getElementById('btn1');
+let ball = document.getElementById('btn1');
+let treats = document.getElementById('btn2');
+let stick = document.getElementById('btn3');
 
-btn.addEventListener('click', (btn) => {
-  console.log('Dogs Deployed!');
+
+ball.addEventListener('click', () => {
+  console.log('Fetch the ball!');
 });
 
+treats.addEventListener('click', () => {
+  console.log('Yum, Yum! Who\'s a good boy!?!?');
+})
+
+stick.addEventListener('click', () => {
+  console.log('Go get the stick!')
+})
